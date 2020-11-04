938eac41185620a304f7c301a73c353695918ac2
Alexandru Burcea <olcitu@gmail.com>
2020-11-04 16:48:32 +0200

    Add files via upload

---
+++ node-red-contrib-sm-rtd/README.md
@@ -0,0 +1,40 @@
+# node-red-contrib-sm-rtd
+
+This is the node-red node to control Sequent Microsystems [RTD Data Acquisition Stackable Card for Raspberry Pi](https://sequentmicrosystems.com/product/rtd-data-acquisition-stackable-card-for-rpi/).
+
+## Install
+
+Clone or update the repository, follow the instrutions fron the [first page.](https://github.com/SequentMicrosystems/rtd-rpi)
+
+In your node-red user directory, tipicaly ~/.node-red,
+
+```bash
+~$ cd ~/.node-red
+```
+
+Run the fallowing command:
+
+```bash
+~/.node-red$ npm install ~/megabas-rpi/node-red-contrib-sm-rtd
+```
+
+In order to see the node in the palette and use-it you need to restart node-red. If you run node-red as a service:
+ ```bash
+ ~$ node-red-stop
+ ~$ node-red-start
+ ```
+
+## Usage
+
+After install and restart the node-red you will see on the node palete, under Sequent Microsystems category one new node:
+
+### rtd node
+
+This node reads one rtd sensor input channel.
+The card stack level and channel number can be set in the node dialog box or dynamically through ```msg.stack``` and ```msg.channel```.
+The read is triggered by the message input and output can be found in the output message payload as a number representing the temperature readings in degree Clsius.
+
+## Important note
+
+This node is using the I2C-bus package from @fivdi, you can visit his work on github [here](https://github.com/fivdi/i2c-bus). 
+The inspiration for this node came from @nielsnl68 work with [node-red-contrib-i2c](https://github.com/nielsnl68/node-red-contrib-i2c).Thank both for the great job.
---
+++ node-red-contrib-sm-rtd/rtd.js
@@ -0,0 +1,79 @@
+module.exports = function(RED) {
+    "use strict";
+    var I2C = require("i2c-bus");
+    // The RTD Node
+    function RTDNode(n) {
+        RED.nodes.createNode(this, n);
+        this.stack = parseInt(n.stack);
+        this.channel = parseInt(n.channel);
+        this.payload = n.payload;
+        this.payloadType = n.payloadType;
+        var node = this;
+        var buffer = Buffer.alloc(4);
+        
+        node.port = I2C.openSync( 1 );
+        node.on("input", function(msg) {
+            var myPayload;
+            var stack = node.stack; 
+            //if (isNaN(stack)) stack = msg.stack;
+            var channel = node.channel || msg.channel;
+            //if (isNaN(channel)) channel = msg.channel;
+            stack = parseInt(stack);
+            channel = parseInt(channel);
+            //var buffcount = parseInt(node.count);
+            if (isNaN(stack+1)) {
+                this.status({fill:"red",shape:"ring",text:"Stack level ("+stack+") value is missing or incorrect"});
+                return;
+            } else if (isNaN(channel) ) {
+                this.status({fill:"red",shape:"ring",text:"Sensor number  ("+channel+") value is missing or incorrect"});
+                return;
+            } else {
+                this.status({});
+            }
+            try {
+                var hwAdd = 0x40;
+                if(stack < 0){
+                    stack = 0;
+                }
+                if(stack > 7){
+                  stack = 7;
+                }
+                hwAdd += stack;
+                
+                if(channel < 1){
+                  channel = 1;
+                }
+                if(channel > 8){
+                  channel = 8;
+                }
+                
+                if (this.payloadType == null) {
+                    myPayload = this.payload;
+                } else if (this.payloadType == 'none') {
+                    myPayload = null;
+                } else {
+                    myPayload = RED.util.evaluateNodeProperty(this.payload, this.payloadType, this,msg);
+                }
+                node.port.readI2cBlock(hwAdd, (channel - 1)*4, 4, buffer,  function(err, size, res) {
+                    if (err) { 
+                        node.error(err, msg);
+                    } 
+                    else{
+                        msg.payload = res.readFloatLE(0).toFixed(4);                       
+                        node.send(msg);
+                    }
+                    });     
+                    
+            } catch(err) {
+                this.error(err,msg);
+            }
+            
+        });
+
+        node.on("close", function() {
+            node.port.closeSync();
+        });
+    }
+    RED.nodes.registerType("rtd", RTDNode);
+
+}
